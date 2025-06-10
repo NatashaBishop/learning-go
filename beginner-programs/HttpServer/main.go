9f2f9ea2cf736f95edd6a4d8cd3c7036f9b9218e
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2025-06-10 23:19:17 +0100

    Update main.go

--- beginner-programs/HttpServer/main.go
+++ beginner-programs/HttpServer/main.go
@@ -7,6 +7,6 @@ import (
 )
 
 func main() {
-	log.Println("Listening on http://localhost:8080/")
+	log.Println("Listening on http://localhost:8080/") //restart the server at another post if needed
 	log.Fatal(http.ListenAndServe(":8080", server.NewServer()))
 }
